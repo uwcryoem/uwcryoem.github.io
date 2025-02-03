75bfab0d5b4ed17b2b74822603d8a3f97b83e9a8
jfscheuren <jfscheuren@wisc.edu>
2025-02-02 19:39:26 -0600

    text changes

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'CryoEM'
+project = 'MCCET'
 copyright = '2025, Jennifer Scheuren'
 author = 'Jennifer Scheuren'
 release = '1.0'
